1c61cfe66ea8560c30fe70ff539b6b0bbe5c7a07
green913 <quinton.helton@mavs.uta.edu>
2016-11-01 22:44:24 -0500

    creating a random part fully implemented

--- main.cpp
+++ main.cpp
@@ -10,43 +10,34 @@ int main()
 
 	Fl_Window *win = new Fl_Window(340, 180);
 
-	//shop.create_rand_part();
-
-	Head *h;
-	Torso *t, *t2;
-	Locomotor *l;
-	Battery *b;
-	Arm *a;
-
-	t = new Torso("Test torso", 123456, 12.5, 12.7, "Testing, testing, 1, 2, 3", 2);
-	shop.add(*t);
+	shop.create_rand_part();
 
 	if (shop.get_available_heads().size() != 0)
 	{
-		*h = shop.get_available_heads()[0];
-		fl_choice(("Random head created:\n" + h->to_string()).c_str(), "OK");
+		Head h = shop.get_available_heads()[0];
+		fl_choice(("Random head created:\n" + h.to_string()).c_str(), "OK", 0, 0);
 	}
 	else if (shop.get_available_torsos().size() != 0)
 	{
-		*t2 = shop.get_available_torsos()[0];
-		string cpp_str_out = "Random torso created:\n" + t2->to_string();
-		char* c_str_out = cpp_str_out.c_str();
+		Torso t2 = shop.get_available_torsos()[0];cout<<"test5\n";
+		string cpp_str_out = "Random torso created:\n" + t2.to_string();
+		const char* c_str_out = cpp_str_out.c_str();cout << "test7\n";
 		fl_choice(c_str_out, "OK", 0, 0);
 	}
 	else if (shop.get_available_locomotors().size() != 0)
 	{
-		*l = shop.get_available_locomotors()[0];
-		fl_choice(("Random locomotor created:\n" + l->to_string()).c_str(), "OK");
+		Locomotor l = shop.get_available_locomotors()[0];
+		fl_choice(("Random locomotor created:\n" + l.to_string()).c_str(), "OK", 0, 0);
 	}
 	else if (shop.get_available_batteries().size() != 0)
 	{
-		*b = shop.get_available_batteries()[0];
-		fl_choice(("Random battery created:\n" + b->to_string()).c_str(), "OK");
+		Battery b = shop.get_available_batteries()[0];
+		fl_choice(("Random battery created:\n" + b.to_string()).c_str(), "OK", 0, 0);
 	}
 	else
 	{
-		*a = shop.get_available_arms()[0];
-		fl_choice(("Random arm created:\n" + h->to_string()).c_str(), "OK");
+		Arm a = shop.get_available_arms()[0];
+		fl_choice(("Random arm created:\n" + a.to_string()).c_str(), "OK", 0, 0);
 	}
 
 	win->end();
