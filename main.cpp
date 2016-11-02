7037d380942f0fb56509f98a3652774e73e6491e
Quinton Helton <quinton.helton@mavs.uta.edu>
2016-11-01 21:59:05 -0500

    Create random part and then display dialog about that part

--- main.cpp
+++ main.cpp
@@ -1,40 +1,51 @@
-/*#include <FL/Fl.H>
+#include <FL/Fl.H>
 #include <FL/Fl_Window.H>
+#include <FL/fl_ask.H>
 
 #include "Controller.h"
 
 int main()
 {
 	Shop shop;
-	
+
 	Fl_Window *win = new Fl_Window(340, 180);
+
+	shop.create_rand_part();
+
+	Head *h;
+	Torso *t;
+	Locomotor *l;
+	Battery *b;
+	Arm *a;
+
+	if (shop.get_available_heads().size() != 0)
+	{
+		*h = shop.get_available_heads()[0];
+		fl_choice(("Random head created:\n" + h->to_string()).c_str(), "OK");
+	}
+	else if (shop.get_available_torsos().size() != 0)
+	{
+		*t = shop.get_available_torsos()[0];
+		fl_choice(("Random torso created:\n" + t->to_string()).c_str(), "OK");
+	}
+	else if (shop.get_available_locomotors().size() != 0)
+	{
+		*l = shop.get_available_locomotors()[0];
+		fl_choice(("Random locomotor created:\n" + l->to_string()).c_str(), "OK");
+	}
+	else if (shop.get_available_batteries().size() != 0)
+	{
+		*b = shop.get_available_batteries()[0];
+		fl_choice(("Random battery created:\n" + b->to_string()).c_str(), "OK");
+	}
+	else
+	{
+		*a = shop.get_available_arms()[0];
+		fl_choice(("Random arm created:\n" + h->to_string()).c_str(), "OK");
+	}
 
 	win->end();
 	win->show();
 
-	return(Fl::run());
-}*/
-
-// All programs include FL/Fl.H
-#include <FL/Fl.H>
-// Include each “widget” you use
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
-int main() 
-{
-	// Instance a new window object
-	Fl_Window *win = new Fl_Window(340, 180);
-	// Add a new box object to the window
-	Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
-	// Set additional fields on the box to specify its appearance
-	box->box(FL_UP_BOX);
-	box->labelfont(FL_BOLD + FL_ITALIC);
-	box->labelsize(36);
-	box->labeltype(FL_SHADOW_LABEL);
-	// Done defining the new Window
-	win->end();
-	// Make the window visible, passing along any user options
-	win->show();
-	// Enter the “command loop” waiting for events
 	return(Fl::run());
 }
